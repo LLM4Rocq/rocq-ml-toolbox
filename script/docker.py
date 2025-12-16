6f78d0f4d1f4036f6a389aa8b4b64ab24d5087cd
theostos <theo.stoskopf@proton.me>
2025-12-16 03:17:21 +0100

    Update docker script to new convention

--- script/docker.py
+++ script/docker.py
@@ -1,14 +1,20 @@
-from src.rocq_ml_toolbox.docker.rocq_docker import RocqDocker, OpamConfig
+from rocq_ml_toolbox.docker.opam_docker import OpamDocker, OpamConfig
 from src.rocq_ml_toolbox.inference.client import PetClient
 
 try:
     opam_config = OpamConfig.from_yaml('docker/config/rocq-test.yaml')
-    rocq_docker = RocqDocker(opam_config)
-    rocq_docker.start_inference_server(port=5000)
+    opam_docker = None
+    opam_docker = OpamDocker(opam_config)
+    opam_docker.start_inference_server(port=5000)
 
     client = PetClient("http://127.0.0.1:5000")
     print("READY")
     print(client.get_session())
+
+    print(opam_docker.list_opam_folder())
+    for package_name in opam_config.packages:
+        print(package_name)
+        print(opam_docker.extract_files(package_name, {'rocq-metarocq': 'MetaRocq'}))
 finally:
-    if rocq_docker is not None:
-        rocq_docker.close()
+    if opam_docker is not None:
+        opam_docker.close()
