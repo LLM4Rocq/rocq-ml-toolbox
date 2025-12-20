6e770888c3df1833f853c793112d943d7c666ad3
theostos <theo.stoskopf@proton.me>
2025-12-20 19:15:28 +0100

    Aligned path convention with physical_path

--- src/rocq_ml_toolbox/parser/parser.py
+++ src/rocq_ml_toolbox/parser/parser.py
@@ -1,9 +1,30 @@
 """Dataclasses and interfaces shared by the parser components."""
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Optional, Union
 
 from pytanque.protocol import Range, Position
+
+@dataclass
+class Notation:
+    """Notation extracted from source file."""
+
+    name: str
+    range: Range
+    path: Optional[str]=None
+    logical_path: Optional[str]=None
+    children: Optional[Union[Element, Notation]]=None
+
+    @classmethod
+    def from_json(cls, d: Dict[str, Any]) -> Element:
+        """Build an element from a dictionary representation."""
+        return cls(
+            path=d.get('path', None),
+            logical_path=d.get('logical_path', None),
+            name=d["name"],
+            range=Range.from_json(d["range"]),
+            children=d.get('children', None)
+        )
 
 @dataclass
 class Element:
@@ -13,16 +34,17 @@ class Element:
     fqn: str
     range: Range
     content: Optional[str]=None
+    content_range: Optional[Range]=None
     kind: Optional[str]=None
-    physical_path: Optional[str]=None
+    path: Optional[str]=None
     logical_path: Optional[str]=None
-    children: Optional[Element]=None
-    
+    children: Optional[Union[Element, Notation]]=None
+
     @classmethod
     def from_json(cls, d: Dict[str, Any]) -> Element:
         """Build an element from a dictionary representation."""
         return cls(
-            physical_path=d.get('physical_path', None),
+            path=d.get('path', None),
             logical_path=d.get('logical_path', None),
             name=d["name"],
             range=Range.from_json(d["range"]),
@@ -86,13 +108,14 @@ class Source:
 
     path: str
     content: str
-
+    logical_path: Optional[str]=None
     @classmethod
     def from_json(cls, d: Dict[str, Any]) -> Source:
         """Build a source from a dictionary representation."""
         return cls(
             path=d["path"],
             content=d["content"],
+            logical_path=d["logical_path"],
         )
 
 class ParserError(Exception):
