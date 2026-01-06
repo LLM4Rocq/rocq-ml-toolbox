05db9ccd6065eb6638ce901d944633c793d51c3a
theostos <theo.stoskopf@proton.me>
2026-01-06 12:55:18 +0100

    Manage is_local condition, update extract_toc to get Inductive, Fixpoint and Notation

--- src/rocq_ml_toolbox/parser/rocq_parser.py
+++ src/rocq_ml_toolbox/parser/rocq_parser.py
@@ -31,15 +31,17 @@ class RocqParser:
     def _extract_blocks(content: str):
         return re.split(r'(?<=\.\s)', content)
 
-    def extract_element(self, state: State, element: str, timeout: int=30, retry=1, is_notation=False) -> Optional[Element]:
-        if is_notation:
-            substate = self.client.run(state, f'About "{element}".', timeout=timeout, retry=retry)
-        else:
-            substate = self.client.run(state, f'About {element}.', timeout=timeout, retry=retry)
-        if substate.feedback:
-            result = substate.feedback[0][1]
-            return parse_about(result, self.map_logical_physical, self.map_physical_logical)
-        return None
+    def extract_element(self, state: State, element: str, timeout: int=30, retry=1, is_notation=False) -> Tuple[Optional[Element], bool]:
+        try:
+            if is_notation:
+                substate = self.client.run(state, f'About "{element}".', timeout=timeout, retry=retry)
+            else:
+                substate = self.client.run(state, f'About {element}.', timeout=timeout, retry=retry)
+            if substate.feedback:
+                result = substate.feedback[0][1]
+                return parse_about(result, self.map_logical_physical, self.map_physical_logical)
+        except ClientError:
+            return None, False
 
     def extract_dependencies(self, state: State, tactic: str, timeout: int=30, retry=1) -> List[Dependency]:
         ast = self.client.ast(state, tactic)
@@ -49,7 +51,7 @@ class RocqParser:
             constants = []
         dependencies = []
         for constant in constants:
-            element = self.extract_element(state, constant, timeout=timeout, retry=retry)
+            element, _ = self.extract_element(state, constant, timeout=timeout, retry=retry)
             if element:
                 dependencies.append(element)
         return dependencies
@@ -71,98 +73,142 @@ class RocqParser:
             state = self.client.get_state_at_pos(source.path, r.end.line, r.end.character)
             yield text, state, r, ast
 
-    def extract_proofs(self, source: Source, timeout=60, retry=1) -> Generator[Theorem, None, None]:
-        fleche_document = self.client.get_document(source.path)
-        steps = None
+    def extract_toc(self, source: Source, timeout=60, retry=1, solve_deps=False) -> Generator[Theorem, None, None]:
+        fleche_document = self.client.get_document(source.path, timeout=timeout)
+        steps = []
+        old_state = self.client.get_root_state(source.path, timeout=timeout, retry=retry)
+        state = old_state
+        initial_goals = None
         for ranged_span in fleche_document.spans[:-1]:
             text = ranged_span.span
             r = ranged_span.range
-            ast = self.client.ast_at_pos(source.path, r.start.line, r.start.character)
-            state = self.client.get_state_at_pos(source.path, r.end.line, r.end.character)
+            ast = self.client.ast_at_pos(source.path, r.start.line, r.start.character, timeout=timeout, retry=retry)
+            old_state = state
+            state = self.client.get_state_at_pos(source.path, r.end.line, r.end.character, timeout=timeout, retry=retry)
             content = ast['v']['expr'][1]
             ast_kind = content[0]
-            
-            if ast_kind in ['VernacExtend', 'VernacBullet', 'VernacEndProof', 'VernacProof']:
+
+            # print(ast_kind)
+            # print(text)
+            hb_detected = False
+            if ast_kind == 'VernacExtend':
+                if 2 <= len(content) and isinstance(content[1], dict) and 'ext_entry' in content[1]:
+                    if content[1]['ext_entry'].startswith('ElpiHB'):
+                        keyword = content[1]['ext_entry'].replace('ElpiHB', 'HB')
+                        hb_detected = True
+                        if keyword != 'HB.export':
+                            try:
+                                ast = self.client.ast(state, text.split(keyword)[1])
+                                content = ast['v']['expr'][1]
+                                ast_kind = content[0]
+                            except ClientError:
+                                pass
+            if not hb_detected and ast_kind in ['VernacExtend', 'VernacBullet', 'VernacEndProof', 'VernacProof', 'VernacSubproof', 'VernacEndSubproof']:
                 goals = self.client.goals(state)
-                if not steps:
-                    steps = []
-                dependencies = self.extract_dependencies(state, text, timeout=timeout, retry=retry)
+                dependencies = []
+                if solve_deps:
+                    dependencies = self.extract_dependencies(state, text, timeout=timeout, retry=retry)
                 step = Step(text, goals, dependencies)
                 steps.append(step)
             else:
                 if steps and name and initial_goals:
-                    element = self.extract_element(state, name, timeout=timeout, retry=retry)
-                    if element:
+                    element, is_local = self.extract_element(old_state, name, timeout=timeout, retry=retry)
+                    if element and is_local:
                         yield Theorem(steps, initial_goals, element)
+                        steps = []
                     else:
-                        print(f"Warning: Ignore element before {text} at {r}")
+                        print(f"Warning: Ignore element before {text} at {r} ({ast_kind})")
             if ast_kind == 'VernacStartTheoremProof':
                 infos = content[2][0][0][0]
                 name = infos['v'][1]
                 initial_goals = self.client.goals(state)
+            elif ast_kind == 'VernacNotation':
+                infos = content[2]
+                name = infos['ntn_decl_string']['v']
+                notation = Notation(name, r, source.path, source.logical_path)
+                yield notation
             elif ast_kind == 'VernacDefinition':
                 infos = content[2][0]
                 name = infos['v'][1][1]
                 initial_goals = self.client.goals(state)
+                element, is_local = self.extract_element(state, name, timeout=timeout, retry=retry)
+                if element and is_local:
+                    yield element
             elif ast_kind == 'VernacInstance':
                 infos = content[1][0]
                 name = infos['v'][1][1]
                 initial_goals = self.client.goals(state)
+                element, is_local = self.extract_element(state, name, timeout=timeout, retry=retry)
+                if element and is_local:
+                    yield element
+            elif ast_kind == 'VernacFixpoint':
+                infos = content[2][1][0]
+                name = infos['fname']['v'][1]
+                element, is_local = self.extract_element(state, name, timeout=timeout, retry=retry)
+                if element and is_local:
+                    yield element
             elif ast_kind == 'VernacInductive':
-                infos = content[2][0][0]
-                name = content[2][0][0][0][1][0]['v'][1]
+                infos = content[2][0][0][0][1][0]
+                name = infos['v'][1]
+                children = []
                 for subinfos in content[2][0][0]:
                     if isinstance(subinfos, list) and subinfos and subinfos[0] == 'Constructors':
                         subinfos = subinfos[1]
                         for constructor in subinfos:
                             constructor = constructor[1][0]
                             constructor_name = constructor['v'][1]
-                            print(constructor_name)
+                            child, is_local = self.extract_element(state, constructor_name, timeout=timeout, retry=retry)
+                            if child and is_local:
+                                child.kind = "Constructor"
+                                children.append(child)
                     if isinstance(subinfos, list) and subinfos and subinfos[0] == 'RecordDecl':
                         subinfos = subinfos[2]
                         for field in subinfos:
                             field = field[0][1]
                             field_name = field['v'][1][1]
-                            bp = field['loc']['bp']
-                            ep = field['loc']['ep']
-                            print(field)
-                            print(field_name)
-                            print(source.content[bp:ep])
-                exit()
+                            child, is_local = self.extract_element(state, field_name, timeout=timeout, retry=retry)
+                            if child and is_local:
+                                child.kind = "Record"
+                                children.append(child)
+                            
+                element, is_local = self.extract_element(state, name, timeout=timeout, retry=retry)
+                if element and is_local:
+                    element.children = children
+                    yield element
 
-    def toc_to_element_tree(self, source: Source, toc_element: TocElement) -> Union[Element, Notation, None]:
-        """
-        Convert a TocElement node (and its TocElement children recursively)
-        into an Element tree using `merge_toc_element` for node conversion.
-        """
-        pos_end = toc_element.range.end
-        state = self.client.get_state_at_pos(source.path, pos_end.line, pos_end.character)
-        is_notation = toc_element.detail=='Notation'
-        if is_notation:
-            return Notation(toc_element.name, toc_element.range, source.path, source.logical_path)
-        element = self.extract_element(state, toc_element.name.v, is_notation=is_notation)
-        if element:
-            element.range = toc_element.range
-            element.kind = toc_element.detail
+    # def toc_to_element_tree(self, source: Source, toc_element: TocElement) -> Union[Element, Notation, None]:
+    #     """
+    #     Convert a TocElement node (and its TocElement children recursively)
+    #     into an Element tree using `merge_toc_element` for node conversion.
+    #     """
+    #     pos_end = toc_element.range.end
+    #     state = self.client.get_state_at_pos(source.path, pos_end.line, pos_end.character)
+    #     is_notation = toc_element.detail=='Notation'
+    #     if is_notation:
+    #         return Notation(toc_element.name, toc_element.range, source.path, source.logical_path)
+    #     element, _ = self.extract_element(state, toc_element.name.v, is_notation=is_notation)
+    #     if element:
+    #         element.range = toc_element.range
+    #         element.kind = toc_element.detail
 
-            if toc_element.children:
-                element.children = [self.toc_to_element_tree(source, child) for child in toc_element.children]
+    #         if toc_element.children:
+    #             element.children = [self.toc_to_element_tree(source, child) for child in toc_element.children]
 
-            return element
-        return None
+    #         return element
+    #     return None
 
-    def extract_toc(self, source: Source, timeout: int=60, retry=1) -> Generator[Union[Theorem, Element, Notation], None, None]:
-        """Read the table of contents for a source file."""   
-        toc = self.client.toc(source.path, timeout=timeout, retry=retry)
-        for _, toc_elements in toc:
-            if not toc_elements:
-                continue
+    # def extract_toc(self, source: Source, timeout: int=60, retry=1) -> Generator[Union[Theorem, Element, Notation], None, None]:
+    #     """Read the table of contents for a source file."""   
+    #     toc = self.client.toc(source.path, timeout=timeout, retry=retry)
+    #     for _, toc_elements in toc:
+    #         if not toc_elements:
+    #             continue
 
-            for toc_element in toc_elements:
-                element_tree = self.toc_to_element_tree(source, toc_element)
-                if element_tree:
-                    print(toc_element)
-                #     yield element_tree
-        exit()
-        for thm in self.extract_proofs(source, timeout=timeout, retry=retry):
-            yield thm
+    #         for toc_element in toc_elements:
+    #             element_tree = self.toc_to_element_tree(source, toc_element)
+    #             if element_tree:
+    #                 print(toc_element)
+    #             #     yield element_tree
+    #     exit()
+    #     for thm in self.extract_proofs(source, timeout=timeout, retry=retry):
+    #         yield thm
