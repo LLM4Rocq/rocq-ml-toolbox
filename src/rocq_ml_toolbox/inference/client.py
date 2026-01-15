af254e9603dc964a6207a58c2a2124acf8b38f62
theostos <theo.stoskopf@proton.me>
2026-01-15 15:24:17 +0100

    Add StateExtended support on client (drop custom State requirement)

--- src/rocq_ml_toolbox/inference/client.py
+++ src/rocq_ml_toolbox/inference/client.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import Dict, Tuple, List, Any, Optional, Set
 import functools
-
+from dataclasses import asdict, dataclass
 import requests
 
 from pytanque.protocol import (
@@ -15,6 +15,36 @@ from pytanque.protocol import (
 
 from ..rocq_lsp.protocol import FlecheDocument
 from ..parser.ast.driver import VernacElement, parse_ast_dump
+
+@dataclass
+class StateExtended(State):
+    generation: int = 0
+    
+    @classmethod
+    def from_json(cls, x: dict) -> StateExtended:
+        base = State.from_json(x)
+        gen = x.get("generation", 0)
+        if not isinstance(gen, int):
+            raise TypeError(f"generation must be int or None, got {type(gen)}")
+
+        return cls(**asdict(base), generation=gen)
+    
+    def to_json(self) -> Any:
+        base = super().to_json()
+        base['generation'] = self.generation
+        return base
+
+    def to_state(self) -> State:
+        return State(
+            self.st,
+            self.proof_finished,
+            self.feedback,
+            self.hash
+        )
+    
+    @classmethod
+    def from_state(cls, base: State, gen: int) -> StateExtended:
+        return cls(**asdict(base), generation=gen)
 
 class ClientError(Exception):
     def __init__(self, code, message):
@@ -42,16 +72,16 @@ def retry(fn):
 
 def check_states(fn):
     """
-    Ensure that all State arguments passed to a function are alive.
+    Ensure that all StateExtended arguments passed to a function are alive.
     """
     @functools.wraps(fn)
     def wrapper(self: PetClient, *args, **kwargs):
         for arg in args:
-            if isinstance(arg, State):
+            if isinstance(arg, StateExtended):
                 self._check_state(arg)
 
         for arg in kwargs.values():
-            if isinstance(arg, State):
+            if isinstance(arg, StateExtended):
                 self._check_state(arg)
 
         return fn(self, *args, **kwargs)
@@ -61,14 +91,14 @@ class PetClient:
     """
     A simple client API for interacting with the pet Flask server.
     """
-    def __init__(self, base_url: str):
+    def __init__(self, host: str, port: int):
         """
         Initialize the client by setting the base URL, logging in, and fetching theorem descriptions.
         """
-        self.base_url = base_url.rstrip("/")
+        self.base_url = f"http://{host}:{port}"
         self.session_id: str = None
-        self.alive_states: Set[State] = set() # keep track of alive intermediate states (run)
-        self.dead_states: Set[State] = set() # keep track of dead intermediate states (each time get_state, or start is called, it killed all previous intermediate states)
+        self.alive_states: Set[StateExtended] = set() # keep track of alive intermediate states (run)
+        self.dead_states: Set[StateExtended] = set() # keep track of dead intermediate states (each time get_state, or start is called, it killed all previous intermediate states)
 
     def connect(self):
         """
@@ -82,7 +112,7 @@ class PetClient:
         else:
             raise ClientError(response.status_code, response.text)
     
-    def _check_state(self, state: State):
+    def _check_state(self, state: StateExtended):
         state_id = state.to_json_string()
         if state_id in self.dead_states:
             raise ClientError(400, 'The given state is dead, did you try to prove simultaneously multiple theorems?')
@@ -91,12 +121,12 @@ class PetClient:
         self.dead_states = self.alive_states | self.dead_states
         self.alive_states = set()
     
-    def _add_state(self, state: State):
+    def _add_state(self, state: StateExtended):
         state_id = state.to_json_string()
         self.alive_states.add(state_id)
 
     @retry
-    def get_state_at_pos(self, filepath: str, line: int, character: int, opts: Optional[Opts]=None, failure: bool=False, timeout: int=120, retry: int=0) -> State:
+    def get_state_at_pos(self, filepath: str, line: int, character: int, opts: Optional[Opts]=None, failure: bool=False, timeout: int=120, retry: int=0) -> StateExtended:
         """
         Get state at position.
         """
@@ -108,38 +138,38 @@ class PetClient:
         if response.status_code == 200:
             output = response.json()
             self._reset_states()
-            return State.from_json(output['resp'])
+            return StateExtended.from_json(output['resp'])
         else:
             raise ClientError(response.status_code, response.text)
 
     @retry
     @check_states
-    def run(self, state: State, tactic: str, opts: Optional[Opts]=None, failure: bool=False, timeout: int=60, retry: int=0) -> State:
+    def run(self, state_ext: StateExtended, tactic: str, opts: Optional[Opts]=None, failure: bool=False, timeout: int=60, retry: int=0) -> StateExtended:
         """
         Execute a given tactic on the current proof state.
         """
         url = f"{self.base_url}/run"
-        state = state.to_json()
+        state_ext = state_ext.to_json()
         opts = opts.to_json() if opts else None
-        payload = {'session_id': self.session_id, 'state': state, 'tactic': tactic, 'opts': opts, 'failure': failure, 'timeout': timeout}
+        payload = {'session_id': self.session_id, 'state_ext': state_ext, 'tactic': tactic, 'opts': opts, 'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
-            state = State.from_json(output['resp'])
-            self._add_state(state)
-            return state
+            state_ext = StateExtended.from_json(output['resp'])
+            self._add_state(state_ext)
+            return state_ext
         else:
             raise ClientError(response.status_code, response.text)
     
     @retry
     @check_states
-    def goals(self, state: State, pretty=True, failure: bool=False, timeout: int=10, retry: int=0) -> List[Goal]:
+    def goals(self, state_ext: StateExtended, pretty=True, failure: bool=False, timeout: int=10, retry: int=0) -> List[Goal]:
         """
         Gather goals associated to a state.
         """
         url = f"{self.base_url}/goals"
-        state = state.to_json()
-        payload = {'session_id': self.session_id, 'state': state, 'pretty': pretty, 'failure': failure, 'timeout': timeout}
+        state_ext = state_ext.to_json()
+        payload = {'session_id': self.session_id, 'state_ext': state_ext, 'pretty': pretty, 'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
@@ -149,29 +179,32 @@ class PetClient:
 
     @retry
     @check_states
-    def complete_goals(self, state: State, pretty=True, failure: bool=False, timeout: int=10, retry: int=0) -> GoalsResponse:
+    def complete_goals(self, state_ext: StateExtended, pretty=True, failure: bool=False, timeout: int=10, retry: int=0) -> Optional[GoalsResponse]:
         """
         Gather complete goals associated to a state.
         """
         url = f"{self.base_url}/complete_goals"
-        state = state.to_json()
-        payload = {'session_id': self.session_id, 'state': state, 'pretty': pretty, 'failure': failure, 'timeout': timeout}
+        state_ext = state_ext.to_json()
+        payload = {'session_id': self.session_id, 'state_ext': state_ext, 'pretty': pretty, 'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
-            return GoalsResponse.from_json(output['resp'])
+            if not output['resp']:
+                return None
+            else:
+                return GoalsResponse.from_json(output['resp'])
         else:
             raise ClientError(response.status_code, response.text)
     
     @retry
     @check_states
-    def premises(self, state: State, failure: bool=False, timeout: int=10, retry: int=0) -> Any:
+    def premises(self, state_ext: StateExtended, failure: bool=False, timeout: int=10, retry: int=0) -> Any:
         """
         Gather accessible premises (lemmas, definitions) from a state.
         """
         url = f"{self.base_url}/premises"
-        state = state.to_json()
-        payload = {'session_id': self.session_id, 'state': state, 'failure': failure, 'timeout': timeout}
+        state_ext = state_ext.to_json()
+        payload = {'session_id': self.session_id, 'state_ext': state_ext, 'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
@@ -181,16 +214,16 @@ class PetClient:
     
     @retry
     @check_states
-    def state_equal(self, st1: State, st2: State, kind=Inspect, failure: bool=False, timeout: int=10, retry: int=0) -> bool:
+    def state_equal(self, st1_ext: StateExtended, st2_ext: StateExtended, kind=Inspect, failure: bool=False, timeout: int=10, retry: int=0) -> bool:
         """
         Check whether state st1 is equal to state st2.
         """
         url = f"{self.base_url}/state_equal"
-        st1 = st1.to_json()
-        st2 = st2.to_json()
+        st1_ext = st1_ext.to_json()
+        st2_ext = st2_ext.to_json()
 
         kind = kind.to_json()
-        payload = {'session_id': self.session_id, 'st1': st1, 'st2': st2, 'kind': kind,'failure': failure, 'timeout': timeout}
+        payload = {'session_id': self.session_id, 'st1_ext': st1_ext, 'st2_ext': st2_ext, 'kind': kind,'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
@@ -200,14 +233,14 @@ class PetClient:
     
     @retry
     @check_states
-    def state_hash(self, state: State, failure: bool=False, timeout: int=10, retry: int=0) -> int:
+    def state_hash(self, state_ext: StateExtended, failure: bool=False, timeout: int=10, retry: int=0) -> int:
         """
         Get a hash value for a proof state.
         """
         url = f"{self.base_url}/state_hash"
-        state = state.to_json()
+        state_ext = state_ext.to_json()
 
-        payload = {'session_id': self.session_id, 'state': state, 'failure': failure, 'timeout': timeout}
+        payload = {'session_id': self.session_id, 'state_ext': state_ext, 'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
@@ -258,13 +291,13 @@ class PetClient:
 
     @retry
     @check_states
-    def ast(self, state: State, text: str, failure: bool=False, timeout: int=10, retry: int=0) -> Dict:
+    def ast(self, state_ext: StateExtended, text: str, failure: bool=False, timeout: int=10, retry: int=0) -> Dict:
         """
         Get ast of a command parsed at a state.
         """
         url = f"{self.base_url}/ast"
-        state = state.to_json()
-        payload = {'session_id': self.session_id, 'state': state, 'text': text, 'failure': failure, 'timeout': timeout}
+        state_ext = state_ext.to_json()
+        payload = {'session_id': self.session_id, 'state_ext': state_ext, 'text': text, 'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
@@ -287,7 +320,7 @@ class PetClient:
             raise ClientError(response.status_code, response.text)
     
     @retry
-    def get_root_state(self, file: str, opts: Optional[Opts]=None, failure: bool=False, timeout: int=10, retry: int=0) -> State:
+    def get_root_state(self, file: str, opts: Optional[Opts]=None, failure: bool=False, timeout: int=10, retry: int=0) -> StateExtended:
         """
         Get root state of a document.
         """
@@ -297,19 +330,19 @@ class PetClient:
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
-            return State.from_json(output['resp'])
+            return StateExtended.from_json(output['resp'])
         else:
             raise ClientError(response.status_code, response.text)
     
     @retry
     @check_states
-    def list_notations_in_statement(self, state: State, statement: str, failure: bool=False, timeout: int=10, retry: int=0) -> list[Dict]:
+    def list_notations_in_statement(self, state_ext: StateExtended, statement: str, failure: bool=False, timeout: int=10, retry: int=0) -> list[Dict]:
         """
         Get the list of notations appearing in a theorem/lemma statement.
         """
         url = f"{self.base_url}/list_notations_in_statement"
-        state = state.to_json()
-        payload = {'session_id': self.session_id, 'state': state, 'statement': statement, 'failure': failure, 'timeout': timeout}
+        state_ext = state_ext.to_json()
+        payload = {'session_id': self.session_id, 'state_ext': state_ext, 'statement': statement, 'failure': failure, 'timeout': timeout}
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             output = response.json()
@@ -318,7 +351,7 @@ class PetClient:
             raise ClientError(response.status_code, response.text)
     
     @retry
-    def start(self, file: str, thm: str, pre_commands: Optional[str]=None, opts: Optional[Opts]=None, failure: bool=False, timeout: int=10, retry: int=0) -> State:
+    def start(self, file: str, thm: str, pre_commands: Optional[str]=None, opts: Optional[Opts]=None, failure: bool=False, timeout: int=10, retry: int=0) -> StateExtended:
         """
         Start a proof session for a specific theorem in a Coq/Rocq file.
         """
@@ -329,7 +362,7 @@ class PetClient:
         if response.status_code == 200:
             output = response.json()
             self._reset_states()
-            return State.from_json(output['resp'])
+            return StateExtended.from_json(output['resp'])
         else:
             raise ClientError(response.status_code, response.text)
 
