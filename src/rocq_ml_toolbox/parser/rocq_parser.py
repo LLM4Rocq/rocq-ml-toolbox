348e2056ca64463819ee2374bf6a4f326e9f897d
theostos <theo.stoskopf@proton.me>
2026-02-02 15:36:34 +0100

    Update extract proof: split between proof_raw and full_proof, fixes issues with kind matching to take into account bullets etc.

--- src/rocq_ml_toolbox/parser/rocq_parser.py
+++ src/rocq_ml_toolbox/parser/rocq_parser.py
@@ -59,6 +59,7 @@ class RocqParser:
         for constant in constants:
             element, _ = self.extract_element(state, constant, timeout=timeout)
             if element:
+                element.name = constant
                 dependencies.append(element)
         return dependencies
 
@@ -77,99 +78,75 @@ class RocqParser:
         for entry in toc:
             entry.data['content'] = content_utf_8[entry.span.bp:entry.span.ep].decode("utf-8")
         return toc
-
-    def extract_proofs(self, source: Source, timeout=120, retry=1, verbose=False, max_consecutive_errors=5) -> Generator[Theorem, None, None]:
-        ast = self.client.get_ast(source.path, retry=retry)
-        proof_open = False
-        initial_goals = None
-        theorem_element = None
-        steps = []
-        namespaces_stack = []
-        state = None
-        consecutive_errors = 0
-        for entry in ast:
-            try:
-                span = entry.span
-                kind = entry.kind
-                subcontent = source.content_utf8[span.bp:span.ep].decode("utf-8")
-                match kind:
-                    case VernacKind.BEGIN_SECTION:
-                        namespaces_stack.append(("SECTION", entry.name))
-                    case VernacKind.DECLARE_MODULE_TYPE| VernacKind.DEFINE_MODULE:
-                        if not entry.data['is_alias']:
-                            namespaces_stack.append(("MODULE", entry.name))
-                    case VernacKind.END_SEGMENT:
-                        last_el = namespaces_stack.pop()
-                        assert entry.name == last_el[1]
-                    case VernacKind.START_THEOREM_PROOF:
-                        proof_open = True
-                        theorem_element = entry
-                        pos = offset_to_pos(source.content_utf8, entry.span.ep)
-                        state = self.client.get_state_at_pos(source.path, pos.line, pos.character, timeout=timeout, retry=retry)
-                        initial_goals = self.client.goals(state)
-                    case VernacKind.PROOF_STEP:
-                        if proof_open:
-                            deps = self.extract_dependencies(state, subcontent)
-                            state = self.client.run(state, subcontent, timeout=timeout, retry=retry)
-                            goals = self.client.goals(state)
-                            step = Step(subcontent, goals, deps)
-                            steps.append(step)
-                            
-                    case VernacKind.END_PROOF:
-                        if proof_open:
-                            stack_modules = [el[1] for el in namespaces_stack if el[0] == 'MODULE']
-                            stack_modules.append(theorem_element.name)
-                            theorem_element.data['fqn'] = ".".join(stack_modules)
-                            yield Theorem(steps, initial_goals, theorem_element)
-                            consecutive_errors = 0
-
-                match kind:
-                    case VernacKind.START_THEOREM_PROOF | VernacKind.PROOF | VernacKind.PROOF_STEP:
-                        pass
-                    case _:
-                        proof_open = False
-                        steps = []
-                        initial_goals = None
-                        theorem_element = None
-            except Exception as e:
-                proof_open = False
-                steps = []
-                initial_goals = None
-                theorem_element = None
-                consecutive_errors += 1
-                with open('parser.log', 'a') as file:
-                    file.write(str(e) + '\n')
-                if max_consecutive_errors < consecutive_errors:
-                    print(f"Too many errors, drop the file {source.path}.")
-                    break
     
-    def extract_proofs_wo_check(self, source: Source) -> List[Tuple[VernacElement, List[str]]]:
+    def extract_proofs_raw(self, source: Source) -> List[Tuple[VernacElement, List[str]]]:
         ast = self.client.get_ast(source.path)
         proof_open = False
         theorem_element = None
         steps = []
         result = []
+        namespaces_stack = []
+        prev_entry = None
         for entry in ast:
             span = entry.span
             kind = entry.kind
             subcontent = source.content_utf8[span.bp:span.ep].decode("utf-8")
             match kind:
+                case VernacKind.BEGIN_SECTION:
+                    namespaces_stack.append(("SECTION", entry.name))
+                case VernacKind.DECLARE_MODULE_TYPE| VernacKind.DEFINE_MODULE:
+                    if not entry.data['is_alias'] and entry.name:
+                        namespaces_stack.append(("MODULE", entry.name))
+                case VernacKind.END_SEGMENT:
+                    last_el = namespaces_stack.pop()
+                    assert entry.name == last_el[1]
                 case VernacKind.START_THEOREM_PROOF:
                     proof_open = True
                     theorem_element = entry
-                case VernacKind.PROOF_STEP:
+                case VernacKind.PROOF:
+                    if not prev_entry:
+                        raise ParserError(f'No prev_entry at {entry.span}')
+                    proof_open = True
+                    theorem_element = prev_entry
+                    steps.append(subcontent)
+                case VernacKind.PROOF_STEP | VernacKind.SUBPROOF | VernacKind.END_SUBPROOF | VernacKind.BULLET:
                     if proof_open:
                         steps.append(subcontent)
-                        
                 case VernacKind.END_PROOF:
                     if proof_open:
+                        stack_modules = [el[1] for el in namespaces_stack if el[0] == 'MODULE']
+                        if not theorem_element.name:
+                            continue
+                        stack_modules.append(theorem_element.name)
+                        theorem_element.data['fqn'] = ".".join(stack_modules)
                         result.append((theorem_element, steps))
-
+                    proof_open = False
+                    steps = []
+                    theorem_element = None
             match kind:
-                case VernacKind.START_THEOREM_PROOF | VernacKind.PROOF | VernacKind.PROOF_STEP:
+                case VernacKind.PROOF | VernacKind.PROOF_STEP | VernacKind.SUBPROOF | VernacKind.END_SUBPROOF | VernacKind.BULLET:
                     pass
+                case VernacKind.START_THEOREM_PROOF:
+                    steps = []
                 case _:
                     proof_open = False
                     steps = []
                     theorem_element = None
+            
+            prev_entry = entry
         return result
+
+    def extract_full_proof(self, source: Source, element: VernacElement, steps_raw: List[str], timeout=60):
+        if not element.span:
+            raise ParserError(f'{element} has no span in {source.path}')
+        pos = offset_to_pos(source.content_utf8, element.span.ep)
+        state = self.client.get_state_at_pos(source.path, pos.line, pos.character, timeout=timeout)
+        initial_goals = self.client.goals(state)
+        steps = []
+        for step_str in steps_raw:
+            deps = self.extract_dependencies(state, step_str)
+            state = self.client.run(state, step_str, timeout=timeout)
+            goals = self.client.goals(state)
+            step = Step(step_str, goals, deps)
+            steps.append(step)
+        return Theorem(steps, initial_goals, element)
