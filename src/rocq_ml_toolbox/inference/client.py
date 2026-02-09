1f852ab02c9bb4958e87a1a00404f0bd55ed4fe6
theostos <theo.stoskopf@proton.me>
2026-02-09 19:06:32 +0100

    add polymorphism for path type

--- src/rocq_ml_toolbox/inference/client.py
+++ src/rocq_ml_toolbox/inference/client.py
@@ -1,6 +1,7 @@
 from pytanque import Pytanque, PytanqueMode
 import requests
-from typing import List, Optional
+from pathlib import Path
+from typing import List, Optional, Union
 
 from ..parser.ast.driver import parse_ast_dump, VernacElement
 from ..parser.glob.driver import GlobFile
@@ -8,15 +9,15 @@ class PytanqueExtended(Pytanque):
     def __init__(self, host: str, port: int):
         super().__init__(host=host, port=port, mode=PytanqueMode.HTTP)
 
-    def get_ast(self, path: str, root: Optional[str]=None, force_dump: bool=False) -> List[VernacElement]:
+    def get_ast(self, path: Union[Path, str], root: Optional[Union[Path, str]]=None, force_dump: bool=False) -> List[VernacElement]:
         url = f"http://{self.host}:{self.port}/get_ast"
-        result = requests.post(url, json={"path":path, "root": root, "force_dump": force_dump})
+        result = requests.post(url, json={"path": str(path), "root": str(root), "force_dump": force_dump})
         raw_ast = result.json()['value']
         return parse_ast_dump(raw_ast)
     
-    def get_glob(self, path: str, force_compile:bool=False) -> GlobFile:
+    def get_glob(self, path: Union[Path, str], force_compile:bool=False) -> GlobFile:
         url = f"http://{self.host}:{self.port}/get_glob"
-        result = requests.post(url, json={"path":path, "force_compile": force_compile})
+        result = requests.post(url, json={"path": str(path), "force_compile": force_compile})
         glob_file = GlobFile.from_json(result.json()['value'])
         return glob_file
     
