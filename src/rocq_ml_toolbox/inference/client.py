27c8012e40e42e1557257a339ab5844f29b1ae35
theostos <theo.stoskopf@proton.me>
2026-03-17 14:53:55 +0100

    feat(client): add proof_dump support to client

--- src/rocq_ml_toolbox/inference/client.py
+++ src/rocq_ml_toolbox/inference/client.py
@@ -6,19 +6,21 @@ from typing import List, Optional, Union, Any, Self, Tuple
 from ..parser.diags.parser import Diagnostic
 from ..parser.ast.driver import parse_ast_dump, VernacElement
 from ..parser.glob.driver import GlobFile
+from ..parser.proof.parser import ProofDump
 
 class PytanqueExtended(Pytanque):
     def __init__(self, host: str, port: int):
         super().__init__(host=host, port=port, mode=PytanqueMode.HTTP)
 
-    def get_ast(self, path: Union[Path, str], root: Optional[Union[Path, str]]=None, force_dump: bool=False) -> Tuple[List[VernacElement], List[Diagnostic]]:
-        url = f"http://{self.host}:{self.port}/get_ast"
+    def get_dump(self, path: Union[Path, str], root: Optional[Union[Path, str]]=None, force_dump: bool=True) -> Tuple[ProofDump, List[VernacElement], List[Diagnostic]]:
+        url = f"http://{self.host}:{self.port}/get_dump"
         if root:
             root = str(root)
         result = requests.post(url, json={"path": str(path), "root": root, "force_dump": force_dump}).json()
-        raw_ast = result['value']
+        raw_proof = result['proof']
+        raw_ast = result['ast']
         diags = result['diags']
-        return parse_ast_dump(raw_ast), [Diagnostic.from_json(d) for d in diags]
+        return ProofDump.from_json(raw_proof), parse_ast_dump(raw_ast), [Diagnostic.from_json(d) for d in diags]
     
     def get_glob(self, path: Union[Path, str], force_compile:bool=False) -> GlobFile:
         url = f"http://{self.host}:{self.port}/get_glob"
