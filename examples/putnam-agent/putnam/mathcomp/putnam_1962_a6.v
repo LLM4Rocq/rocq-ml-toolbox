28487f360657f3a5fb7d01da23b7867983dc9029
theostos <theo.stoskopf@proton.me>
2026-04-09 08:45:49 +0200

    fix putnam bench statement (conjonction/disjonction mismatch)

--- examples/putnam-agent/putnam/mathcomp/putnam_1962_a6.v
+++ examples/putnam-agent/putnam/mathcomp/putnam_1962_a6.v
@@ -3,6 +3,6 @@ Theorem putnam_1962_a6
     (A : Ensemble Q)
     (hSSadd : forall a b : Q, (A a /\ A b) -> A (a + b))
     (hSSprod : forall a b : Q, (A a /\ A b) -> A (a * b))
-    (hSScond : forall r : Q, (A r \/ A (-r) \/ r = 0) /\ ~(A r \/ A (-r)) /\ ~(A r /\ r = 0) /\ ~(A (-r) /\ r = 0))
+    (hSScond : forall r : Q, (A r \/ A (-r) \/ r = 0) /\ ~(A r /\ A (-r)) /\ ~(A r /\ r = 0) /\ ~(A (-r) /\ r = 0))
     : A = (fun r : Q => r > 0).
 Proof. Admitted.
